3165f2551748b24db5681aea6203d940905f9559
nadarabea1 <nadarabea81@gmail.com>
2023-12-26 15:49:45 +0200

    Added Lab1

---
+++ Lab1/script.js
@@ -0,0 +1,14 @@
+var firstName= prompt("Enter your first Name :")
+var lastName= prompt("Enter your last Name :")
+var fullName= `${firstName} ${lastName}`
+
+var age =Number(prompt("Enter your birth year: "))
+
+document.write(`Welcome, ${fullName}.\nYou are ${2023-age} years old.`)
+document.write(`<br>`)
+
+
+var num1=Number(prompt("Enter the first number: "))
+var num2=Number(prompt("Enter the second number: "))
+
+document.write(`${num1} + ${num2} = ${num1+num2}`)
