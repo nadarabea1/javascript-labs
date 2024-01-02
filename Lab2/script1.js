c8816885bca395a459e41831cab97db0ad49b820
nadarabea1 <nadarabea81@gmail.com>
2024-01-02 15:43:34 +0200

    Edited script1

--- Lab2/script1.js
+++ Lab2/script1.js
@@ -1,24 +1,27 @@
 do{
-    var age=prompt("Enter your age");
-}while(age<=0)
+    do{
+        var age=prompt("Enter your age");
+        console.log(age)
+    }while(age<=0)
+    
+    if(age >=1 && age <=10){
+        document.write("Child")
+        document.write(`</br>`)
+    }
+    else if(age >=11 && age <=18){
+        document.write("Teenager")
+        document.write(`</br>`)
 
-if(age >=1 && age <=10){
-    document.write("Child")
-    document.write(`</br>`)
-}
-else if(age >=11 && age <=18){
-    document.write("Teenager")
-    document.write(`</br>`)
+    }
+    else if(age >=19 && age <=50){
+        document.write("Grown up")
+        document.write(`</br>`)
 
-}
-else if(age >=19 && age <=50){
-    document.write("Grown up")
-    document.write(`</br>`)
+    }
+    else if(age >50){
+        document.write("Old")
+        document.write(`</br>`)
 
-}
-else if(age >50){
-    document.write("Old")
-    document.write(`</br>`)
-
-}
-
+    }
+    var ans= confirm("Do you want ot exit")
+}while(!ans)
