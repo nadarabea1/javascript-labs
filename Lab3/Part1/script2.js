30c102338c1ba853a16f810faff70bcb481083c6
nadarabea1 <nadarabea81@gmail.com>
2024-01-03 16:42:15 +0200

    Edited part1 with bouns

--- Lab3/Part1/script2.js
+++ Lab3/Part1/script2.js
@@ -1,27 +1,33 @@
+function cal(num1,num2){
+    var result;
+    if(!isNaN(num1) && !isNaN(num2)){
+    if(op=="sum"){
+        result=num1+num2
+    }
+    else if(op=="subtract"){
+        result=num1-num2
+    }
+    else if(op=="division"){
+        result=num1/num2
+    }
+    else if(op=="moduls"){
+        result=num1%num2
+    }
+    else{
+        document.write(`Invaild operation `,`<br/>`)
+    
+    }
+    }
+    return result;
+}
+
 var num1=Number(prompt("Enter Number 1"))
-var op=prompt("Enter Operation(sum - subtract - division - moduls)")
-var num2=Number(prompt("Enter Number 2"))
-var result
-
-if(!isNaN(num1) && !isNaN(num2)){
-if(op=="sum"){
-    result=num1+num2
-    document.write(`Sumation= ${num1}+${num2} = ${result}`)
-}
-else if(op=="subtract"){
-    result=num1-num2
-    document.write(`Subtraction= ${num1}-${num2} = ${result}`)
-}
-else if(op=="division"){
-    result=num1/num2
-    document.write(`division= ${num1}/${num2} = ${result}`)
-}
-else if(op=="moduls"){
-    result=num1%num2
-    document.write(`Moduls= ${num1}%${num2} = ${result}`)
-}
-else{
-    document.write(`Invaild operation`)
-
-}
-}
+var result=num1
+do{
+    var op=prompt("Enter Operation(sum - subtract - division - moduls)")
+    var num2=Number(prompt("Enter Number 2"))
+    result=cal(result,num2)
+    var ans=confirm("Do you continue?")
+    
+}while(ans)
+document.write(`Result = ${result}<br/>`)
